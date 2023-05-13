2aba4844307260451f68502dac6f4dbc8853c81a
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-05-13 14:47:20 +0200

    squares.py added

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 def square(x):
     return x * x
-
+"""
 for i in range(10):
-    print(f"The sqaure of {i} is {square(i)}")
+    print(f"The square of {i} is {square(i)}")
+"""
---
+++ squares.py
@@ -0,0 +1,10 @@
+# import modality 1
+# from functions import square
+# import modality 2
+import functions
+
+for i in range(10):
+    # import mod 1 use:
+    # print(f"The square of {i} is {square(i)}")
+    # import mod 2 use:
+    print(f"The square of {i} is {functions.square(i)}")
