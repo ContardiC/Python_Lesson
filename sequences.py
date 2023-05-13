12e39e6766d20d6c11e1bf12355514adc03bbce2
Carlo Contardi <carlo.contardi@isisbernocchi.edu.it>
2023-05-13 12:44:45 +0200

    defintions added

--- sequences.py
+++ sequences.py
@@ -1,5 +1,14 @@
 name = "Gino"
 print(name[0])
+#list - sequence of mutable values
 names = ["Puffo", "Nino"]
 print(names[0])
+#tuple - sequence of immutable values
+coordinateX = 10.0
+coordinateY = 20.0
 
+coordinate = (10.0, 20.0)
+
+#set - collectio of unique values
+
+#dict - collection of key-value pairs
